afed7c31711a8ea99ea9d900116ac941be4b8a87
Stangar999 <lockinout@mail.ru>
2023-01-08 21:23:34 +0300

    with gtest

--- test_version.cpp
+++ test_version.cpp
@@ -1,12 +1,24 @@
-﻿#define BOOST_TEST_MODULE test_version
+﻿//#define BOOST_TEST_MODULE test_version
 #include "lib.h"
-#include <boost/test/included/unit_test.hpp>
+//#include <boost/test/included/unit_test.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+TEST(TestVersion, Subtest_1) {
+  ASSERT_TRUE(version() > 0);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
+
+//BOOST_AUTO_TEST_SUITE(test_version)
+
+//BOOST_AUTO_TEST_CASE(test_valid_version)
+//{
+//    BOOST_CHECK(version() > 0);
+//}
+
+//BOOST_AUTO_TEST_SUITE_END()
